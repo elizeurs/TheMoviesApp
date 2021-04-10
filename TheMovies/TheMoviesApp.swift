7513c95bff6826eeeaca2ba9b08d56ac76d25dfe
elizeurs <13ders@gmail.com>
2021-04-10 19:33:49 -0300

    Initial Commit

---
+++ TheMovies/TheMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMoviesApp.swift
+//  TheMovies
+//
+//  Created by Elizeu RS on 10/04/21.
+//
+
+import SwiftUI
+
+@main
+struct TheMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
