bc6a59ec910c3cea7480613a98e232f01776f765
elizeurs <13ders@gmail.com>
2021-04-10 21:24:27 -0300

    Home Tab View

---
+++ TheMovies/Views/DiscoverView.swift
@@ -0,0 +1,20 @@
+//
+//  DiscoverView.swift
+//  TheMovies
+//
+//  Created by Elizeu RS on 10/04/21.
+//
+
+import SwiftUI
+
+struct DiscoverView: View {
+    var body: some View {
+        Text("Hello, Discover!")
+    }
+}
+
+struct DiscoverView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiscoverView()
+    }
+}
---
+++ TheMovies/Views/MoviesView.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesView.swift
+//  TheMovies
+//
+//  Created by Elizeu RS on 10/04/21.
+//
+
+import SwiftUI
+
+struct MoviesView: View {
+    var body: some View {
+        Text("Hello, Movies!")
+    }
+}
+
+struct MoviesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoviesView()
+    }
+}
