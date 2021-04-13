d512bd753381a15a075a3aa33b66eaa0107cf31c
elizeurs <13ders@gmail.com>
2021-04-13 17:27:38 -0300

    Movie Detail View

--- TheMovies/Views/MoviesView.swift
+++ TheMovies/Views/MoviesView.swift
@@ -71,7 +71,7 @@ struct MoviesView: View {
         ForEach(movieManager.movies.filter {
                   searchTerm.isEmpty ? true : $0.title?.lowercased().localizedStandardContains(searchTerm.lowercased()) ?? true }) { movie in
           NavigationLink(
-            destination: Text(movie.titleWithLanguage)) {
+            destination: MovieDetailView(movie: movie)) {
 //            Text(movie.titleWithLanguage)
             MovieCell(movie: movie)
           }.listRowBackground(Color.clear)
